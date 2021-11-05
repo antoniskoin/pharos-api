bbb6097243d770f9b0a4bb5896ff42b8bfaeeee5
antonis <antonykoinousis@gmail.com>
2021-11-05 10:15:01 +0200

    Add blueprint template

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_restful import Api
 from Pharmacies import PharmaciesToday, SpecificAreaPharmacy
 from Hospitals import HospitalsToday
 
 app = Flask(__name__)
 api = Api(app)
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 
 api.add_resource(PharmaciesToday.PharmacyInformation, '/pharmacies/today')
