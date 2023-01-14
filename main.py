65d949b1b56f0c06467b6832e4bbdc45cca07195
antonis <antonykoinousis@gmail.com>
2023-01-14 20:37:30 +0200

    Minor changes

--- main.py
+++ main.py
@@ -1,20 +1,20 @@
 from flask import Flask, render_template
 from flask_restful import Api
-from Pharmacies import PharmaciesToday, SpecificAreaPharmacy
-from Hospitals import HospitalsToday
+from Pharmacies import pharmacies_today, specific_area_pharmacy
+from Hospitals import hospitals_today
 
 app = Flask(__name__)
 api = Api(app)
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-api.add_resource(PharmaciesToday.PharmacyInformation, '/pharmacies/today')
-api.add_resource(SpecificAreaPharmacy.SpecificPharmaciesToday, '/pharmacies/specific_today')
-api.add_resource(HospitalsToday.HospitalInformationToday, '/hospitals/today')
+api.add_resource(pharmacies_today.PharmacyInformation, "/pharmacies/today")
+api.add_resource(specific_area_pharmacy.SpecificPharmaciesToday, "/pharmacies/specific_today")
+api.add_resource(hospitals_today.HospitalInformationToday, "/hospitals/today")
 
 if __name__ == "__main__":
     app.run()
